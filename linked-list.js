bcf79e6b16d2f1ecff1be94720c64ccc0b36bfb5
aidataml <52681210+aidataml@users.noreply.github.com>
2023-11-06 15:59:43 -0500

    Initial upload of linked lists.

---
+++ linked-list.js
@@ -0,0 +1,171 @@
+/** Node: node for a singly linked list. */
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+/** LinkedList: chained together nodes. */
+
+class LinkedList {
+  constructor(vals = []) {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+
+    for (let val of vals) this.push(val);
+  }
+
+  /** _get(idx): retrieve node at idx. */
+
+  _get(idx) {
+    let count = 0;
+    let current = this.head;
+
+    while (current !== null && count != idx) {
+      count += 1;
+      current = current.next;
+    }
+
+    return current;
+  }
+
+  /** push(val): add new value to end of list. Appends a new node with value val to the tail. Returns undefined.*/
+
+  push(val) {
+    let newNode = new Node(val);
+
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+
+    this.length += 1;
+  }
+
+  /** unshift(val): add new value to start of list. Add a new node with value val to the head. Returns undefined.*/
+
+  unshift(val) {
+    let newNode = new Node(val);
+
+    if (this.head === null) {
+      this.head = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+
+    if (this.length === 0) this.tail = this.head;
+
+    this.length += 1;
+  }
+
+  /** pop(): return & remove last item. Remove & return tail value. Throws error if list is empty.*/
+
+  pop() {
+    return this.removeAt(this.length - 1);
+  }
+
+  /** shift(): return & remove first item. Remove & return head value. Throws error if list is empty.*/
+
+  shift() {
+    return this.removeAt(0);
+  }
+
+  /** getAt(idx): get val at idx. Retrieve value at index position idx. Throws error if index is invalid.*/
+
+  getAt(idx) {
+    if (idx >= this.length || idx < 0) {
+      throw new Error("Invalid index.");
+    }
+
+    return this._get(idx).val;
+  }
+
+  /** setAt(idx, val): set val at idx to val. Set value of node at index position idx to val. Throws error if index is invalid.*/
+
+  setAt(idx, val) {
+    if (idx >= this.length || idx < 0) {
+      throw new Error("Invalid index.");
+    }
+
+    let current = this._get(idx);
+    current.val = val;
+  }
+
+  /** insertAt(idx, val): add node w/val before idx. Insert a new node at position idx with value val. Throws error if index is invalid. Returns undefined.*/
+
+  insertAt(idx, val) {
+    if (idx > this.length || idx < 0) {
+      throw new Error("Invalid index.");
+    }
+
+    if (idx === 0) return this.unshift(val);
+    if (idx === this.length) return this.push(val);
+
+    let prev = this._get(idx - 1);
+
+    let newNode = new Node(val);
+    newNode.next = prev.next;
+    prev.next = newNode;
+
+    this.length += 1;
+  }
+
+  /** removeAt(idx): return & remove item at idx. Remove & return value at position idx. Throws error if index is invalid.*/
+
+  removeAt(idx) {
+    if (idx > this.length || idx < 0) {
+      throw new Error("Invalid index.");
+    }
+
+    // If idx is 0, at the head of a linked list, remove the index and value of the head.
+    if (idx === 0) {
+      let val = this.head.val;
+      this.head = this.head.next;
+      this.length -= 1;
+      if (this.length < 2) this.tail = this.head;
+      return val;
+    }
+
+    // If idx is the tail (length - 1), remove the index and value of the tail.
+    if (idx === this.length - 1) {
+      let val = prev.next.val;
+      prev.next = null;
+      this.tail = prev;
+      this.length -= 1;
+      return val;
+    }
+
+    let prev = this._get(idx - 1);
+
+    // Remove value at a middle index
+    let val = prev.next.val;
+    prev.next = prev.next.next;
+    this.length -= 1;
+    return val;
+  }
+
+  /** average(): return an average of all values in the list */
+
+  average() {
+    if (this.length === 0) return 0;
+
+    let current = this.head;
+    let total = 0;
+
+    while (current) {
+      total += current.val;
+      current = current.next;
+    }
+
+    return total / this.length;
+  }
+}
+
+module.exports = LinkedList;
